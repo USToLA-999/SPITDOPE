276e882abc77c75113c5f7bdb4a6b23a000f8477
sunnysingh191934 <sunny.zeelab@gmail.com>
2025-06-11 01:56:41 +0530

    insert video

--- src/Pages/Home/Hero.jsx
+++ src/Pages/Home/Hero.jsx
@@ -1,13 +1,13 @@
+import banner from  '../../assets/bannervideo.mp4'
 
-import image from '../../assets/group.png'
 
 const Hero = () => {
     return (
         <div className="main-container-box hero-box">
             <div className="container pt-5 pb-5 mx-auto ">
                 <div className="socials-box-corner h-full w-full">
-                    {/* <video src={videobaner} muted loop autoPlay></video> */}
-                    <img src={image} alt="" />
+                    <video src={banner} muted loop autoPlay></video>
+                    
                 </div>
             </div>
         </div>
