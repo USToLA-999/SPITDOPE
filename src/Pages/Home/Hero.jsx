8feba6b05b72c745e947028a205596ece343ee72
sunnysingh191934 <sunny.zeelab@gmail.com>
2025-06-11 11:15:55 +0530

    fix path

--- src/Pages/Home/Hero.jsx
+++ src/Pages/Home/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
             <div className="container pt-5 pb-5 mx-auto ">
                 <div className="socials-box-corner h-full w-full">
                     <video autoPlay muted loop >
-                        <source src='/public/bannervideo.mp4' type="video/mp4" />
+                        <source src='/bannervideo.mp4' type="video/mp4" />
                     </video>
                     
                 </div>
