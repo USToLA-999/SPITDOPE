967b2d565c5a465f92a3bda1f721a4cbf6c2efc0
sunnysingh191934 <sunny.zeelab@gmail.com>
2025-06-11 12:54:56 +0530

    add mobile banner and hide desktop banner

--- src/Pages/Home/Hero.jsx
+++ src/Pages/Home/Hero.jsx
@@ -5,9 +5,15 @@ const Hero = () => {
     return (
         <div className="main-container-box hero-box">
             <div className="container  mx-auto ">
-                <div className="socials-box-corner h-full w-full">
-                    <video autoPlay muted loop playsInline >
+                <div className="socials-box-corner sm:flex hidden h-full w-full">
+                    <video autoPlay muted loop playsInline className="" >
                         <source src='https://res.cloudinary.com/dz1dqwf2y/video/upload/v1749622257/xuxp6zylbfsytosgtx7t.mp4' type="video/mp4" />
+                    </video>
+                    
+                </div>
+                <div className="socials-box-corner sm:hidden flex h-full w-full">
+                    <video autoPlay muted loop playsInline className="" >
+                        <source src='https://res.cloudinary.com/dz1dqwf2y/video/upload/v1749626394/bannerphone_1_hq6snd.mp4' type="video/mp4" />
                     </video>
                     
                 </div>
