17b12a5955d6728cf1fa1410be34e8a6391bd452
sunnysingh191934 <sunny.zeelab@gmail.com>
2025-06-11 11:37:26 +0530

    Re-add banner video without LFS

--- src/Pages/Home/Hero.jsx
+++ src/Pages/Home/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <div className="main-container-box hero-box">
             <div className="container pt-5 pb-5 mx-auto ">
                 <div className="socials-box-corner h-full w-full">
-                    <video autoPlay muted loop >
+                    <video autoPlay muted loop playsInline >
                         <source src='/bannervideo.mp4' type="video/mp4" />
                     </video>
                     
